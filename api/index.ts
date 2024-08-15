aeaa8a88aa119a5443030d6f420611355e0c9f05
Thai Nguyen <nh0kcrazy04@gmail.com>
2024-08-15 21:09:55 +0700

    abcdef

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,4 @@
-const express = require("express");
-const app = express();
+import app from "../index.js";
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
